72e45810973e587638c1a9a8f88413e632d04bc1
zxg <1547968110@qq.com>
2024-03-24 00:18:51 +0800

    新增查询教学计划树接口

--- zhiqiantong_plus_content/zhiqiantong_plus_content_api/src/main/java/com/zhiqiantong/content/api/TeachplanController.java
+++ zhiqiantong_plus_content/zhiqiantong_plus_content_api/src/main/java/com/zhiqiantong/content/api/TeachplanController.java
@@ -1,5 +1,9 @@
 package com.zhiqiantong.content.api;
 
+import com.zhiqiantong.content.model.dto.TeachplanDto;
+import io.swagger.annotations.ApiImplicitParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.zhiqiantong.content.service.TeachplanService;
@@ -7,6 +11,8 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,9 +23,18 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 @Slf4j
 @RestController
-@RequestMapping("teachplan")
+@RequestMapping("/teachplan")
+@Api(value = "课程计划编辑接口",tags = "课程计划编辑接口")
 public class TeachplanController {
 
     @Autowired
     private TeachplanService  teachplanService;
+
+    @ApiOperation("查询课程计划树形结构")
+    @ApiImplicitParam(value = "courseId",name = "课程Id",required = true,dataType = "Long",paramType = "path")
+    @GetMapping("/{courseId}/tree-nodes")
+    public List<TeachplanDto> getTreeNodes(@PathVariable Long courseId){
+        return teachplanService.getTreeNodes(courseId);
+    }
+
 }
