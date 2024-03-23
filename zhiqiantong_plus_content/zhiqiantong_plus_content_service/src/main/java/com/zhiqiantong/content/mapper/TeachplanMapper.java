3bfef7823424e6f751af76aad68eb3e5e0a12220
zxg <1547968110@qq.com>
2024-03-24 00:19:29 +0800

    实现查询教学计划树接口

--- zhiqiantong_plus_content/zhiqiantong_plus_content_service/src/main/java/com/zhiqiantong/content/mapper/TeachplanMapper.java
+++ zhiqiantong_plus_content/zhiqiantong_plus_content_service/src/main/java/com/zhiqiantong/content/mapper/TeachplanMapper.java
@@ -1,8 +1,11 @@
 package com.zhiqiantong.content.mapper;
 
+import com.zhiqiantong.content.model.dto.TeachplanDto;
 import com.zhiqiantong.content.model.po.Teachplan;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * <p>
@@ -12,5 +15,14 @@ import org.springframework.stereotype.Repository;
  * @author zxg
  */
 public interface TeachplanMapper extends BaseMapper<Teachplan> {
+
+    /**
+     * @description 查询某课程的课程计划，组成树型结构
+     * @param courseId
+     * @return com.xuecheng.content.model.dto.TeachplanDto
+     * @author Mr.M
+     * @date 2022/9/9 11:10
+     */
+    public List<TeachplanDto> selectTreeNodes(long courseId);
 
 }
